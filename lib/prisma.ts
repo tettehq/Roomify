40bd44c75fe79d2d6553e493d079f9d6537363c8
AndrewJHalisky <ajhalisky@gmail.com>
2026-09-18 11:07:00 -0400

    Fixed portions of projects with errors

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,16 @@
-import "dotenv/config"
-import { PrismaClient } from "../generated/prisma/client"
+import * as PrismaClientPackage from "@prisma/client"
+
+const PrismaClient = (PrismaClientPackage as typeof PrismaClientPackage & {
+    PrismaClient: new (options?: { datasourceUrl?: string }) => PrismaClientInstance
+}).PrismaClient
+
+type PrismaClientInstance = InstanceType<
+    typeof PrismaClientPackage extends { PrismaClient: infer T }
+        ? T extends new (...args: any[]) => infer I
+            ? new (...args: any[]) => I
+            : never
+        : never
+>
 
 const prisma = new PrismaClient({
     datasourceUrl: process.env.DATABASE_URL,
